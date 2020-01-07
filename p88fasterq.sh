2015609cf89c1392c41f16835540c966dcab95fb
lucky5sugar <lucky5sugar@gmail.com>
2020-01-07 15:30:14 +0900

    Update p88fasterq.sh

--- p88fasterq.sh
+++ p88fasterq.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #DRY analysis p.88
 
-cat SRR_Acc_List.txt | \
-\
 while read line
 do
-  cmd="fasterq-dump --split-files ${line}
+#download sra files and compress those files
+  cmd="fasterq-dump --split-files ${line};\
   gzip ${line}*fastq"
   eval ${cmd}
-done
+done <SRR_Acc_List.txt
