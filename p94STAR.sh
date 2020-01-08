48e4018292a8b61dd967612a2e9612184533b0a1
lucky5sugar <lucky5sugar@gmail.com>
2020-01-08 18:42:25 +0900

    Update p94STAR.sh
    Fixed basename error "extra operand"
    Use fastq files instead of fastq.gz files

--- p94STAR.sh
+++ p94STAR.sh
@@ -3,14 +3,13 @@
 
 module load STAR
 
-for sample in `ls ../seq/*fastq.gz | xargs basename | cut -f1 -d"_" | uniq`
+for sample in `ls ../seq/*.fastq | xargs basename -a | cut -f1 -d"_" | uniq`
 do
 echo mapping:${sample}
 STAR --runMode alignReads --genomeDir ../ref/STAR_reference \
---readFilesCommand gunzip -c --readFilesIn ../seq/${sample}_1.fastq.gz \
+--readFilesIn ../seq/${sample}_1.fastq.gz \
 ../seq/${sample}_2.fastq.gz --outSAMtype BAM SortedByCoordinate --runThreadN 12 \
 --quantMode TranscriptomeSAM --outFileNamePrefix ${sample}
 done
 
 echo finished
-
