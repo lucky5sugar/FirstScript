f874cfbc99854ed76c493bd6f799bbc8203582c7
lucky5sugar <lucky5sugar@gmail.com>
2020-01-08 18:49:59 +0900

    Update p94STAR.sh
    Removed .gz

--- p94STAR.sh
+++ p94STAR.sh
@@ -7,8 +7,8 @@ for sample in `ls ../seq/*.fastq | xargs basename -a | cut -f1 -d"_" | uniq`
 do
 echo mapping:${sample}
 STAR --runMode alignReads --genomeDir ../ref/STAR_reference \
---readFilesIn ../seq/${sample}_1.fastq.gz \
-../seq/${sample}_2.fastq.gz --outSAMtype BAM SortedByCoordinate --runThreadN 12 \
+--readFilesIn ../seq/${sample}_1.fastq \
+../seq/${sample}_2.fastq --outSAMtype BAM SortedByCoordinate --runThreadN 12 \
 --quantMode TranscriptomeSAM --outFileNamePrefix ${sample}
 done
 
